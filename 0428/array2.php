e8ffb7c56f4587ded8d6bd83360cdf4def5d8e7d
takeo0124 <takeo.yuka.0124@gmail.com>
2025-04-28 16:27:14 +0900

    for文array02

---
+++ 0428/array2.php
@@ -0,0 +1,18 @@
+<?php
+$name = [
+    'sato' => '佐藤',
+    'suzuki' => '鈴木',
+    'takahashi' => '高橋'
+];
+
+var_dump($name);
+
+echo $name['takahashi'] . "<br>";
+
+foreach ($name as $value) {
+    echo $value . "<br>";
+}
+
+foreach ($name as $key => $value) {
+    echo 'キーは' . $key . "、名前は" . $value . "<br>";
+}
